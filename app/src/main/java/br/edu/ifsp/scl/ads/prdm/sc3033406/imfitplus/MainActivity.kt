1b427d4eddf074ba28e8ffa3c63ca36b889c0dcc
Vinicius64 <oliveira.vinicius64@gmail.com>
2025-10-26 09:06:16 -0300

    feat: initialize ImFitPlus project with basic structure and tests

---
+++ app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3033406/imfitplus/MainActivity.kt
@@ -0,0 +1,13 @@
+package br.edu.ifsp.scl.ads.prdm.sc3033406.imfitplus
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+    }
+}
