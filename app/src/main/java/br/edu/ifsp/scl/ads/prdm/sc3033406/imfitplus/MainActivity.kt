13908f61453bf87171ffc2d8e877d4e1d5182387
Vinicius64 <oliveira.vinicius64@gmail.com>
2025-10-26 09:16:12 -0300

    feat: enable view binding in MainActivity and update layout reference

--- app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3033406/imfitplus/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3033406/imfitplus/MainActivity.kt
@@ -3,11 +3,16 @@ package br.edu.ifsp.scl.ads.prdm.sc3033406.imfitplus
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import br.edu.ifsp.scl.ads.prdm.sc3033406.imfitplus.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val amb : ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(amb.root)
     }
 }
